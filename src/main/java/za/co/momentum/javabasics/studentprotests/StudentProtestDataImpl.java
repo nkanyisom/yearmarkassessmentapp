c88c2382c239ffd91bef3eab625c2e6f10c9f8cd
bongani1mas <bongani169@gmail.com>
2021-04-12 14:05:55 +0200

    Update StudentProtestDataImpl.java

--- src/main/java/za/co/momentum/javabasics/studentprotests/StudentProtestDataImpl.java
+++ src/main/java/za/co/momentum/javabasics/studentprotests/StudentProtestDataImpl.java
@@ -18,38 +18,6 @@ public class StudentProtestDataImpl implements StudentProtestData {
     @Override
     public String getAnnualProtestsReport(Date startDate, Date endDate) {
 
-        String fileLocation = "src/main/resources/studentProtestData.txt";
-        int count = 0;
-        BufferedReader reader;
-        List<String> studentProtestData = new ArrayList<>();
 
-        try {
-            reader = new BufferedReader(new FileReader(fileLocation));
-            String line = reader.readLine();
-            while(line != null) {
-                line = reader.readLine();
-                studentProtestData.add(line);
-                count++;
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String[] studentProtest = studentProtestData.toArray(new String[studentProtestData.size()]);
-
-
-        return studentProtest;
     }
 }
-/*
-    BufferedReader abc = new BufferedReader(new FileReader(myfile));
-    List<String> lines = new ArrayList<String>();
-
-while((String line = abc.readLine()) != null) {
-        lines.add(line);
-        System.out.println(data);
-        }
-        abc.close();
-
-// If you want to convert to a String[]
-        String[] data = lines.toArray(new String[]{});*/
